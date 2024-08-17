9139b5c9e720bbb6dc6181ac402b1f5d18f22e7e
Gustavo Martins <gustavommartinsctg@outlook.com>
2024-08-17 03:57:58 -0300

    feat: add spring project

---
+++ src/main/java/br/com/gummart/spring/api/calculator/SpringApiCalculatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.gummart.spring.api.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiCalculatorApplication.class, args);
+	}
+
+}
