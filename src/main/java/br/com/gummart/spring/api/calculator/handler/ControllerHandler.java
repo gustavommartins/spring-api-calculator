3a9bdffc5fcdee644cd1cfe9cacd0100d3fea29b
Gustavo Martins <gustavommartinsctg@outlook.com>
2024-08-23 00:03:52 -0300

    feat: Add bad request handler

--- src/main/java/br/com/gummart/spring/api/calculator/handler/ControllerHandler.java
+++ src/main/java/br/com/gummart/spring/api/calculator/handler/ControllerHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.Date;
 
@@ -17,6 +18,15 @@ public class ControllerHandler {
                 .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
                 .data(new Date().toString())
                 .erro("Ocorreu um erro interno no servidor verifique os logs!")
+                .build());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    ResponseEntity<Object> handleException(final MethodArgumentTypeMismatchException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ResponseError.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .data(new Date().toString())
+                .erro("O parâmetro de calculo deve ser um número, parâmetro com erro: " + ex.getValue())
                 .build());
     }
 
